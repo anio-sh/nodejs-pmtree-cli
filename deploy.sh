8b2213ad7877c238b3a2c3b98beab418ba705155
dasdeo <dasdeo@proton.me>
2023-11-27 08:00:38 +0100

    Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ curl \
 	--request POST \
 	--data-binary "@./dist/pmtree.mjs" \
 	-H "Content-Type:application/octet-stream" \
-	-H "x-anio-auth-key: $ANIO_DEPLOY_KEY" \
+	-H "x-anio-auth-key: $ANIO_SH_DEPLOY_KEY" \
 	-H "x-anio-file-name: pmtree" \
-	https://anio.sh/upload
+	"$ANIO_SH_DEPLOY_URL"
