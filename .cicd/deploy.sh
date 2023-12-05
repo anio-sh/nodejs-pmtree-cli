243ded7e747419ed04104b8490cd0ca53c1d940d
dasdeo <dasdeo@proton.me>
2023-12-05 07:32:01 +0100

    Fix wrong paths in ci/cd deployment script (doh!).

--- .cicd/deploy.sh
+++ .cicd/deploy.sh
@@ -2,13 +2,13 @@
 
 printf "Deploying with version '%s'\n" "$RELEASE_VERSION"
 
-rm -f ./dist/tpcfggen.mjs
+rm -f ./dist/pmtree.mjs
 
 ./node_modules/.bin/anio_bundler .
 
 curl \
 	--request POST \
-	--data-binary "@./dist/tpcfggen.mjs" \
+	--data-binary "@./dist/pmtree.mjs" \
 	-H "Content-Type:application/octet-stream" \
 	-H "x-anio-auth-key: $ANIO_SH_DEPLOY_KEY" \
 	-H "x-anio-file-name: pmtree" \
